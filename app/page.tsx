0fdd6f60142293600462b0a125a5d725f7920440
Mikias Berhane <158381691+m8nt0@users.noreply.github.com>
2025-04-07 13:51:21 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,10 +43,9 @@ export default function Home() {
             />
             <div>
               <p className="text-lg mb-4">
-                I'm a student and visionary technologist, dedicated to creating computing systems that work to create self-sustaining and self-organizing systems. My goal is to develop technologies that minimize harm to our planet and actively contribute to its ever-adaptive cycle.
-              </p>
+                Student and Visionary technologist, dedicated to creating/optimizing computing systems balancing self-sustaining and self-organizing systems. 
               <p className="text-lg">
-                With a background in computer science, I'm continuously learning about the four primary subjects through the lens of nature.
+                Applying biological principles to design decentralized, adaptive systems that harmonize human governance, computing, and ecology.
               </p>
             </div>
           </div>
